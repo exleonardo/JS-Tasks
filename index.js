f4cefb1d7b640fd2e058393741a0727f4b035309
Alex Khomenok <exleonardo@mail.ru>
2023-02-13 21:57:52 +0300

    next task

--- index.js
+++ index.js
@@ -249,3 +249,9 @@ function mango(quantity, price) {
 }
 
 //!-------------------------------------------
+
+//https://www.codewars.com/kata/5625618b1fe21ab49f00001f/solutions/javascript
+function sayHello(name) {
+	return "Hello, " + name;
+}
+//!--------------------------------------------
